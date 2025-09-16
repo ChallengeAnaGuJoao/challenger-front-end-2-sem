d4e85f756526baa265da8e1bb077492e7d95192b
gkenji110 <gusavo.terada01@gmail.com>
2025-09-16 15:09:00 -0300

    refactor: adjusting responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Header } from "./components/header";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Header />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  <>
+  </>
   );
 }
 
